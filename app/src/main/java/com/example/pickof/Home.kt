8a09bdce7526e5361c99117618d3b182e365bcf2
wda <vorobieva2005dasha@gmail.com>
2024-11-22 23:34:48 +0700

    navigation base

---
+++ app/src/main/java/com/example/pickof/Home.kt
@@ -0,0 +1,212 @@
+package com.example.pickof
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.pickof.ui.theme.Biryuzovyi
+import com.example.pickof.ui.theme.Seryi
+import com.example.pickof.ui.theme.Typography
+import com.example.pickof.ui.theme.gamaamli
+
+@Composable
+fun Home(navController: NavHostController){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Seryi
+    )
+    { innerPadding ->
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            colors = CardDefaults.cardColors(Seryi)
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(bottom = 60.dp)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text("PICK PICK PICK PICK PICK",
+                    modifier = Modifier
+                        .padding(horizontal = 25.dp),
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontFamily = gamaamli,
+                    fontSize = 30.sp,
+                    lineHeight = 37.sp,
+                    softWrap = true
+                )
+
+                //1
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 1",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //2
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 2",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //3
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 3",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //4
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 4",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //5
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 5",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //6
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 6",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //7
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 7",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //8
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 8",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+                //9
+                Button(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+//                        navController.navigate("home")
+                    }
+                ) {
+                    Text("path 9",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
+                }
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/pickof/MainActivity.kt
+++ app/src/main/java/com/example/pickof/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.pickof
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +17,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +25,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.pickof.ui.theme.Biryuzovyi
 import com.example.pickof.ui.theme.PickOfTheme
 import com.example.pickof.ui.theme.Seryi
@@ -37,67 +41,83 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PickOfTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    containerColor = Seryi
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "startpick"
+                ){
+                    composable("startpick"){
+                        MA(navController)
+                    }
+                    composable("home"){
+                        Home(navController)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MA (navController: NavHostController) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Seryi
+    )
+    { innerPadding ->
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            colors = CardDefaults.cardColors(Seryi)
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "RAEWDA PRODUCTION",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    style = Typography.bodySmall,
+                    color = Color.White
                 )
-                { innerPadding ->
-                   Card(
-                       modifier = Modifier
-                           .fillMaxSize()
-                           .padding(innerPadding),
-                       colors = CardDefaults.cardColors(Seryi)
-                   ){
-                       Column(
-                           modifier = Modifier
-                               .fillMaxSize(),
-                           verticalArrangement = Arrangement.SpaceBetween,
-                           horizontalAlignment = Alignment.CenterHorizontally
-                       ) {
 
-                           Text(
-                               text = "RAEWDA PRODUCTION",
-                               modifier = Modifier
-                                   .fillMaxWidth(),
-                               style = Typography.bodySmall,
-                               color = Color.White
-                           )
+                Image(
+                    painter = painterResource(R.drawable.iccon),
+                    contentDescription = "img",
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .size(370.dp),
+                )
 
-                            Image(
-                                painter = painterResource(R.drawable.iccon),
-                                contentDescription = "img",
-                                alignment = Alignment.Center,
-                                modifier = Modifier
-                                    .size(370.dp),
-                            )
+                Text("PICK AND HAVE TIME, CLOSE ALL PLANS\n PICK PICK PICK PICK PICK",
+                    modifier = Modifier
+                        .padding(horizontal = 25.dp),
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontFamily = gamaamli,
+                    fontSize = 36.sp,
+                    lineHeight = 37.sp,
+                    softWrap = true
+                )
 
-                           Text("PICK AND HAVE TIME, CLOSE ALL PLANS\n PICK PICK PICK PICK PICK",
-                               modifier = Modifier
-                                   .padding(horizontal = 25.dp),
-                               color = Color.White,
-                               textAlign = TextAlign.Center,
-                               fontFamily = gamaamli,
-                               fontSize = 36.sp,
-                               lineHeight = 37.sp,
-                               softWrap = true
-                               )
-
-                           Button(
-                               modifier = Modifier
-                                   .padding(bottom = 60.dp)
-                                   .size(120.dp),
-                               colors = ButtonDefaults.buttonColors(Biryuzovyi),
-                               onClick = {
-                                   
-                               }
-                           ) {
-                               Text("PICK",
-                                   modifier = Modifier,
-                                   style = Typography.bodySmall,
-                                   color = Color.White)
-                           }
-                       }
-                   }
+                Button(
+                    modifier = Modifier
+                        .padding(bottom = 60.dp)
+                        .size(120.dp),
+                    colors = ButtonDefaults.buttonColors(Biryuzovyi),
+                    onClick = {
+                        navController.navigate("home")
+                    }
+                ) {
+                    Text("PICK",
+                        modifier = Modifier,
+                        style = Typography.bodySmall,
+                        color = Color.White)
                 }
             }
         }
